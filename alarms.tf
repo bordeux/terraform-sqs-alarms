67726892abb7664ee1f40fd64272d420c0019f07
Chris Bednarczyk <chris@tourradar.com>
2019-10-16 08:33:07 +0200

    Use checks with priority  instead of static value

--- alarms.tf
+++ alarms.tf
@@ -1,14 +1,14 @@
 resource "aws_cloudwatch_metric_alarm" "ApproximateNumberOfMessagesNotVisible" {
-  count               = var.approximate_number_messages_not_visible_enabled ? 1 : 0
-  alarm_name          = "${var.alarm_prefix}: Approximate Number Of Messages Not Visible is high for ${var.sqs_name}"
+  for_each            = var.approximate_number_messages_not_visible_checks
+  alarm_name          = "${var.alarm_prefix}: ${each.key} Approximate Number Of Messages Not Visible is high for ${var.sqs_name}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = var.approximate_number_messages_not_visible_periods
-  threshold           = var.approximate_number_messages_not_visible_threshold
+  threshold           = each.value
   metric_name         = "ApproximateNumberOfMessagesNotVisible"
   namespace           = "AWS/SQS"
   period              = var.approximate_number_messages_not_visible_period
   statistic           = "Average"
-  alarm_description   = "Alarm is above of threshold: ${var.approximate_number_messages_not_visible_threshold}. Looks like Queue Consumer cannot process messages"
+  alarm_description   = "Priority: ${each.key} Alarm is above of threshold: ${each.value}. Looks like Queue Consumer cannot process messages"
   treat_missing_data  = var.approximate_number_messages_not_visible_missing_data
   alarm_actions       = var.actions
   tags                = var.tags
@@ -19,16 +19,16 @@ resource "aws_cloudwatch_metric_alarm" "ApproximateNumberOfMessagesNotVisible" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "ApproximateNumberOfMessagesVisible" {
-  count               = var.approximate_number_messages_visible_enabled ? 1 : 0
-  alarm_name          = "${var.alarm_prefix}: Approximate Number Of Messages Visible is high for ${var.sqs_name}"
+  for_each            = var.approximate_number_messages_visible_checks
+  alarm_name          = "${var.alarm_prefix}: ${each.key} Approximate Number Of Messages Visible is high for ${var.sqs_name}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = var.approximate_number_messages_visible_periods
-  threshold           = var.approximate_number_messages_visible_threshold
+  threshold           = each.value
   metric_name         = "ApproximateNumberOfMessagesVisible"
   namespace           = "AWS/SQS"
   period              = var.approximate_number_messages_visible_period
   statistic           = "Average"
-  alarm_description   = "Alarm is above of threshold: ${var.approximate_number_messages_visible_threshold}. Looks like Queue Consumer is too slow to process messages"
+  alarm_description   = "Priority: ${each.key} Alarm is above of threshold: ${each.value}. Looks like Queue Consumer is too slow to process messages"
   treat_missing_data  = var.approximate_number_messages_visible_missing_data
   alarm_actions       = var.actions
   tags                = var.tags
@@ -41,16 +41,16 @@ resource "aws_cloudwatch_metric_alarm" "ApproximateNumberOfMessagesVisible" {
 
 
 resource "aws_cloudwatch_metric_alarm" "ApproximateAgeOfOldestMessage" {
-  count               = var.approximate_age_of_oldest_message_enabled ? 1 : 0
-  alarm_name          = "${var.alarm_prefix}: Approximate Age Of Oldest Message is high for ${var.sqs_name}"
+  for_each            = var.approximate_age_of_oldest_message_checks
+  alarm_name          = "${var.alarm_prefix}: ${each.key} Approximate Age Of Oldest Message is high for ${var.sqs_name}"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = var.approximate_age_of_oldest_message_periods
-  threshold           = var.approximate_age_of_oldest_message_threshold
+  threshold           = each.value
   metric_name         = "ApproximateAgeOfOldestMessage"
   namespace           = "AWS/SQS"
   period              = var.approximate_age_of_oldest_message_period
   statistic           = "Maximum"
-  alarm_description   = "Alarm is above of threshold: ${var.approximate_age_of_oldest_message_threshold}. Looks like Queue Consumer is too slow to process messages"
+  alarm_description   = "Priority: ${each.key} Alarm is above of threshold: ${each.value}. Looks like Queue Consumer is too slow to process messages"
   treat_missing_data  = var.approximate_age_of_oldest_message_missing_data
   alarm_actions       = var.actions
   tags                = var.tags
--- vars.tf
+++ vars.tf
@@ -15,14 +15,15 @@ variable "tags" {
 }
 
 
-variable "approximate_number_messages_not_visible_enabled" {
-  type    = bool
-  default = true
-}
-
-variable "approximate_number_messages_not_visible_threshold" {
-  type    = number
-  default = 10000
+variable "approximate_number_messages_not_visible_checks" {
+  type = map(number)
+  default = {
+    P1 = 5000
+    P2 = 4000
+    P3 = 3000
+    P4 = 2000
+    P5 = 1000
+  }
 }
 
 variable "approximate_number_messages_not_visible_periods" {
@@ -41,19 +42,21 @@ variable "approximate_number_messages_not_visible_missing_data" {
 }
 
 
-variable "approximate_number_messages_visible_enabled" {
-  type    = bool
-  default = true
+
+variable "approximate_number_messages_visible_checks" {
+  type = map(number)
+  default = {
+    P1 = 25000
+    P2 = 20000
+    P3 = 15000
+    P4 = 10000
+    P5 = 5000
+  }
 }
 
 variable "approximate_number_messages_visible_missing_data" {
   type    = string
   default = "notBreaching"
-}
-
-variable "approximate_number_messages_visible_threshold" {
-  type    = number
-  default = 10000
 }
 
 variable "approximate_number_messages_visible_periods" {
@@ -67,21 +70,25 @@ variable "approximate_number_messages_visible_period" {
 }
 
 
-variable "approximate_age_of_oldest_message_enabled" {
-  type    = bool
-  default = true
+
+variable "approximate_age_of_oldest_message_checks" {
+  type = map(number)
+  default = {
+    P1 = 60 * 60 * 5 # 5 hour
+    P2 = 60 * 60 * 4 # 4 hour
+    P3 = 60 * 60 * 3 # 3 hour
+    P4 = 60 * 60 * 2 # 2 hour
+    P5 = 60 * 60     # 1 hour
+  }
+  description = "In seconds"
 }
+
 
 variable "approximate_age_of_oldest_message_missing_data" {
   type    = string
   default = "notBreaching"
 }
 
-variable "approximate_age_of_oldest_message_threshold" {
-  type        = number
-  default     = 60 * 60 # 1 hour
-  description = "In seconds"
-}
 
 variable "approximate_age_of_oldest_message_periods" {
   type    = number
